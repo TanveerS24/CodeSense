7da4e2d8f148b7801f43b963a0ed62332fe9e7dd
TanveerS24 <stanveer1809@gmail.com>
2026-04-03 21:10:15 +0530

    Minor changes

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -1,11 +1,12 @@
 'use client';
 
 import Navbar from './Navbar';
+import { ReactNode } from 'react';
 
 export default function LayoutWrapper({
   children,
 }: {
-  children: React.ReactNode;
+  children: ReactNode;
 }) {
   return (
     <>
